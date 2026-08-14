9da8a1f0700e8e8171569056c3b82f85f3dd541e
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-14 19:17:39 +0200

    created AuthConfig and get for movies  v2

--- src/main/java/adastra/backend/controllers/MovieController.java
+++ src/main/java/adastra/backend/controllers/MovieController.java
@@ -1,4 +1,26 @@
 package adastra.backend.controllers;
 
+import adastra.backend.entities.Movie;
+import adastra.backend.services.MoviesService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/movies")
+@AllArgsConstructor
 public class MovieController {
+
+    private MoviesService moviesService;
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Page<Movie> getMovies(@RequestParam(defaultValue = "0") int page,
+                                 @RequestParam(defaultValue = "10") int size,
+                                 @RequestParam(defaultValue = "title") String searchBy) {
+        
+        return this.moviesService.findAll(page, size, searchBy);
+    }
 }
--- src/main/java/adastra/backend/services/MoviesService.java
+++ src/main/java/adastra/backend/services/MoviesService.java
@@ -1,4 +1,23 @@
 package adastra.backend.services;
 
-public class MovieService {
+import adastra.backend.entities.Movie;
+import adastra.backend.repository.MoviesRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MoviesService {
+
+    private MoviesRepository moviesRepository;
+
+    public Page<Movie> findAll(int page, int size, String searchBy) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(searchBy));
+        return this.moviesRepository.findAll(pageable);
+    }
+
 }
