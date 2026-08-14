41f4ae5b8ac93557d7d78fc3e42385d5b0dc4e99
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-14 11:49:27 +0200

    created Seat entity

---
+++ src/main/java/adastra/backend/entities/Seat.java
@@ -0,0 +1,37 @@
+package adastra.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "seats")
+public class Seat {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private UUID id;
+    @Column(nullable = false)
+    private char row;
+    @Column(nullable = false)
+    private Integer number;
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Screen screenId;
+    @Column(nullable = false)
+    private String color;
+
+
+    public Seat(char row, Integer number, Screen screenId, String color) {
+        this.row = row;
+        this.number = number;
+        this.screenId = screenId;
+        this.color = color;
+    }
+
+}
