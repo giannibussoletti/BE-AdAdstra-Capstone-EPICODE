3dcdbe15f8fbac98fa7c6154085b30a38ee0dd1a
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-14 11:23:51 +0200

    created cinema entity

---
+++ src/main/java/adastra/backend/entities/Cinema.java
@@ -0,0 +1,35 @@
+package adastra.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "cinemas")
+public class Cinema {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private UUID id;
+
+    @Column(name = "cinema_name", nullable = false)
+    private String cinemaName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City cityId;
+
+    public Cinema(String cinemaName, String address, City cityId) {
+        this.cinemaName = cinemaName;
+        this.address = address;
+        this.cityId = cityId;
+    }
+}
