d791d073f6cbcee9fb0d0ec35abed9a5eb951341
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-14 11:27:52 +0200

    created screen entity

---
+++ src/main/java/adastra/backend/entities/Screen.java
@@ -0,0 +1,25 @@
+package adastra.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "cinemas")
+public class Screen {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private UUID id;
+
+    @Column(name = "screen_number", nullable = false)
+    private Integer screenNumber;
+
+    @JoinColumn(name = "cinema_id", nullable = false)
+    private Cinema cinemaId;
+}
