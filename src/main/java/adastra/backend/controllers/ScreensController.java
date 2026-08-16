9b4bf0f54fd54a67fdf4b53160d0fe6e32e7c3bb
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-16 16:05:38 +0200

    created all controllers

--- src/main/java/adastra/backend/controllers/ScreensController.java
+++ src/main/java/adastra/backend/controllers/ScreensController.java
@@ -1,16 +1,15 @@
 package adastra.backend.controllers;
 
-import adastra.backend.DTO.ResponseDTO;
-import adastra.backend.DTO.ScreenDTO;
+import adastra.backend.DTO.*;
 import adastra.backend.entities.Screen;
 import adastra.backend.services.ScreensService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/screens")
@@ -19,8 +18,23 @@ public class ScreensController {
 
     private ScreensService screensService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseDTO save(@Valid @RequestBody ScreenDTO body) {
         Screen saved = this.screensService.save(body);
         return new ResponseDTO("Sala salvata correttamente", saved.getId(), LocalDateTime.now());
+    }
+
+    @PatchMapping("/{screenId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateScreen(@Valid @RequestBody ScreenUpdateDTO body, @PathVariable UUID screenId) {
+        this.screensService.screenUpdate(body, screenId);
+    }
+
+    @PatchMapping("/remove/{screenId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDeleteDTO softDelete(@RequestBody DeleteDTO body, @PathVariable UUID screenId) {
+        this.screensService.softDeleteGeneric(screenId, body.deletion());
+        return new ResponseDeleteDTO("Stato della sala aggiornato correttamente", LocalDateTime.now());
     }
 }
